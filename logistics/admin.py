8ece696980d4ee972c4b29c328ddb4c7b3534a58
kimedwardcollines <kimedwardcollines@users.noreply.github.com>
2026-05-16 17:37:57 +0000

    Register Booking model in admin panel

--- logistics/admin.py
+++ logistics/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Booking
 
 # Register your models here.
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ('name', 'service_type', 'phone', 'email', 'pickup_location', 'destination', 'date', 'created_at')
+    list_filter = ('service_type', 'created_at')
+    search_fields = ('name', 'phone', 'email')
